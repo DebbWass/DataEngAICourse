71f116d93572af03aa0bc7f45495d25b41580be4
Dorit W <DorWass@gmail.com>
2025-12-19 23:08:27 +0200

    Python exersices Fron JB Course

--- EX01Q04-Q06.py
+++ EX01Q04-Q06.py
@@ -1,10 +1,11 @@
 a = int(input("insert first number "))
 b= int(input("insert second number "))
-
+"""
 c= a+b
 d= a-b
 e= a*b
 f= a//b
+"""
 """
 print("c =", int(c))
 print ("d =", int(d))
@@ -22,11 +23,33 @@ print(f" a * b |   {e}")
 print(f" a // b |  {f}")
 print("-----------------------------")
 """
-
+"""
 print("-" * 60)
 print(f"{'a':>5} | {'b':>5} | {'c=a+b':>5} | {'d=a-b':>5} | {'e=a*b':>5} | {'f=a//b':>5}")
 print("-" * 60)
 print(f"{a:>5} | {b:>5} | {c:>5} | {d:>5} | {e:>5} | {f:>5}")
+print("-" * 60)
+"""
+
+print("\n" + "-" * 60)
+print(f"{'a':>5} | {'b':>5} | {'a+b':>5} | {'a-b':>5} | {'a*b':>5} | {'a//b':>5}")
+print("-" * 60)
+
+# חישוב והדפסת כל פעולה בעמודה משלה, תוך שימוש רק ב-c
+c = a + b
+col1 = f"{c:>5}"
+
+c = a - b
+col2 = f"{c:>5}"
+
+c = a * b
+col3 = f"{c:>5}"
+
+c = a // b
+col4 = f"{c:>5}"
+
+# הדפסת שורת התוצאות
+print(f"{a:>5} | {b:>5} | {col1} | {col2} | {col3} | {col4}")
 print("-" * 60)
 
 
