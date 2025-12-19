8030e319b8a50bfd9840d3b26fa5abe052bcc30f
Dorit W <DorWass@gmail.com>
2025-12-19 23:10:49 +0200

    Python exersices Fron JB Course

---
+++ .venv/EX01Q07-09.py
@@ -0,0 +1,44 @@
+"""
+#Q07
+a = int(input("insert first number:"))
+b= int(input("insert second number:"))
+c= int(input("insert third number:"))
+
+print(a + b+ c)
+
+"""
+
+# Q08
+"""
+arr = []
+
+for i in range(3) :
+    num = int(input("Insert a number"))
+    arr.append(num)
+
+sum = 0
+
+for i in range(len(arr)) :
+    sum= sum + arr[i]
+
+print(sum)
+"""
+"""
+#Q08-2
+
+sum = 0
+
+for i in range(3):
+    sum = sum + int(input("Insert a number"))
+
+print(sum)
+"""
+#Q09
+
+sum = 0
+
+for i in range(5):
+    sum = sum + int(input("Insert a number"))
+
+print(sum)
+
